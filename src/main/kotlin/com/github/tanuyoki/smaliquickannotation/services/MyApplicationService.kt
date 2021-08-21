9c6f2dfe5776670821f40bc67f1e0d5e585a9700
GitHub Action <action@github.com>
2021-08-21 13:50:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tanuyoki/smaliquickannotation/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tanuyoki.smaliquickannotation.services
+
+import com.github.tanuyoki.smaliquickannotation.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/tanuyoki/smaliquickannotation/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tanuyoki/smaliquickannotation/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tanuyoki.smaliquickannotation.services
 
+import com.github.tanuyoki.smaliquickannotation.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
